f3bc24ca7173a23081c0675e60b56ac0b6d3b35e
Dibas Dauliya <dibasdauliya@gmail.com>
2022-06-10 20:12:43 +0545

    Update tailwind

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import logo from './logo.svg'
-import './App.css'
-
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <h1 className='font-2xl font-bold'>React.js + Tailwind CSS</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+    <main className='bg-gray-800 text-white h-screen'>
+      <div className='grid place-items-center h-full'>
+        <div className='space-y-4'>
+          <h1 className='text-3xl font-bold'>React.js + Tailwind CSS</h1>
+          <p>
+            Edit <code className='bg-gray-700 rounded-sm p-1'>src/App.js</code>{' '}
+            and save to reload.
+          </p>
+          <a
+            className='underline'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Learn React
+          </a>
+        </div>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {},
   },
-  variants: {
-    extend: {}
-  },
-  plugins: []
-}
+  plugins: [],
+};
